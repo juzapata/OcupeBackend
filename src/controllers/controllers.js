a6e197b9b9e606fc1d17ee103716d8b1d076db23
juzapata <juliamzapata@gmail.com>
2021-01-16 16:42:02 -0300

    rota para buscar mentorados modificada para buscar através do email

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -18,21 +18,21 @@ router.get('/get-mentors', async (req, res) => {
 
 /*get /get-mentored
     body: {
-        id: 1
+        email: laura@gmail.com
     }
 */
 router.get('/get-mentored', async (req, res) => {
     
     try {
-        if (!req.body.id){
+        if (!req.body.email){
             return res.status(400).send({sucesso: false, erro: "é necessário colocar o id do mentorado no body"});
         }
-        let id = req.body.id;
+        let email = req.body.email;
         let mentorado = Mentored.filter(person =>{
-            return person.id == id;
+            return person.contact.email == email;
         });
         if (!mentorado){
-            return res.status(400).send({sucesso: false, erro: "mentorado não existe"});
+            return res.status(400).send({sucesso: false, erro: "mentorad@ não existe"});
         }
 
         return res.status(200).send({sucesso: true, mentorado: mentorado[0]});
@@ -45,51 +45,6 @@ router.get('/get-mentored', async (req, res) => {
 
 }); 
 
-/* post /set-status/ 
-    body: {
-        id: 1
-        stage: "mentoria"
-        status: "true"
-    } 
-    */
-router.post('/set-status', async (req, res)=>{
-    try {
-        let id = req.body.id;
-        let stage = req.body.stage;
-        let status = req.body.status;
 
-        if (!id){
-            return res.status(400).send({sucesso: false, erro: "é necessário colocar o id do mentorado no body"});
-        }
-        if (!stage){
-            return res.status(400).send({sucesso: false, erro: "é necessário colocar o stage do mentorado a ser setado"});
-        }
-        if (!status){
-            return res.status(400).send({sucesso: false, erro: "é necessário colocar se o status é true ou false"});
-        }
-        
-        let mentorado = Mentored.filter(person =>{
-            return person.id == id;
-        });
-        if (!mentorado){
-            return res.status(400).send({sucesso: false, erro: "mentorado não existe"});
-        }
-
-        for (etapa in mentorado[0].stages){
-            if (etapa == stage){
-                console.log(etapa);
-                console.log(etapa.value);
-                etapa.value = status;
-            }
-        }
-        // console.log(mentorado[0]);
-
-        return res.status(200).send({sucesso: true, mentorado: mentorado[0]});
-
-    } catch (error) {
-        console.log('ERRR', error);
-        return res.status(400).send({error}); 
-    }
-})
 
 module.exports = app => app.use('/ocupe', router);
